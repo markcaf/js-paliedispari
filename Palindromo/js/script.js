cda7dfb2908ee177b26aa4bf76fff304407c983a
markcaf <marcocafassog@gmail.com>
2022-06-22 16:54:14 +0200

    Declaration of variables for the word and reverse

---
+++ Palindromo/js/script.js
@@ -0,0 +1,8 @@
+// Dichiaro la variabile della parola che dovrà inserire l'utente
+const userWord = prompt("Inserisci una parola");
+
+// Dichiaro la variabile per usare il singolo carattere della parola
+let char;
+
+// Dichiaro la variabile per la parola inversa
+let invertedWord = "";
