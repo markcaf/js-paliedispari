cbfd52a7deb8d19cd2a415a936821857b35f59a1
markcaf <marcocafassog@gmail.com>
2022-06-22 18:05:59 +0200

    Added control over user inputs

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,7 +1,17 @@
 // L'utente sceglie se vuole pari o dispari
-const choice = prompt("Scrivi la tua scelta: 'pari' o 'dispari'");
+let choice = prompt("Scrivi la tua scelta: 'pari' o 'dispari'");
+while ((choice != "pari") && (choice != "dispari")){
+    choice = prompt("Scrivi 'pari' o 'dispari'");
+}
+
 console.log("Hai scelto: " + choice);
 
 // L'utente inserisce un numero da 1 a 5
-const userNumber = parseInt( prompt("Inserisci un numero da 1 a 5"));
+let userNumber = parseInt( prompt("Inserisci un numero da 1 a 5"));
+while ((userNumber < 1 ) || (userNumber > 5)){
+    userNumber = prompt("Inserisci un numero da 1 a 5");
+}
 console.log("Hai scelto: " + userNumber);
+
+
+
