d4df5ba589160c2d171e3d52322de74670bb7bcc
markcaf <marcocafassog@gmail.com>
2022-06-22 17:25:42 +0200

    User input for the number and "even or odd"

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,7 @@
+// L'utente sceglie se vuole pari o dispari
+const choice = prompt("Scrivi la tua scelta: 'pari' o 'dispari'");
+console.log("Hai scelto: " + choice);
+
+// L'utente inserisce un numero da 1 a 5
+const userNumber = parseInt( prompt("Inserisci un numero da 1 a 5"));
+console.log("Hai scelto: " + userNumber);
